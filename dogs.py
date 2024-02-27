41d51d31cd0695386667ce5af2a5819180593d55
Roel D'Haese <roel.dhaese@gmail.com>
2024-02-28 00:26:16 +0100

    another last try

--- dogs.py
+++ dogs.py
@@ -29,7 +29,7 @@ left_column, middle_column, right_column = st.columns([3, 1, 1])
 ########
 
 
-with open("C:/Users/Roel/Desktop/my-first-streamlitapp/data/georef-switzerland-kanton.geojson", 'r') as response:
+with open("./data/georef-switzerland-kanton.geojson", 'r') as response:
     electricity = json.load(response)
 
 canton_names = {
